4ff1a15df2c6ad3893e0d9bbf95fab52c9a31167
Mauricio Santamaria <mtrejosantamaria@gmail.com>
2019-02-15 14:54:54 -0600

    La función de búsqueda de línea híbrida está lista, también subí las funciones para probarla

--- Proyecto 1/metodoblhibrido.m
+++ Proyecto 1/metodoblhibrido.m
@@ -1,4 +1,4 @@
-function [x,iter] = MetodoBL(fname,x)
+function [x,iter] = metodoblhibrido(fname,x)
 % Método de búsqueda de línea con la primer condición de Wolfe
 % usando máximo descenso.
 % 13 de febrero de 2019.
@@ -18,44 +18,75 @@ function [x,iter] = MetodoBL(fname,x)
 % g = gradiente (fname,x);
 %------------------------------------------------------------------------
 
-% parámetros
-tol     = 1.e-04; % tolerancia para la norma del gradiente.
-c1      = 0.1;    % valor para la condición de Wolfe
-maxiter = 50;    % número máximo de iteraciones externas permitidas
-maxjter = 30;     % número máximo de iteraciones internas de BL permitidas
+% Parámetros
+tol     = 1.e-08; % Tolerancia para la norma del gradiente.
+c1      = 1e-04;    % Valor para la condición de Wolfe
+maxiter = 250;    % Número máximo de iteraciones externas permitidas
+maxjter = 20;     % Número máximo de iteraciones internas de BL permitidas
 
-% valores iniciales
-iter = 0;        % contador para las iteraciones externas
+% Valores iniciales
+iter = 0;        % Contador para las iteraciones externas
 fx = feval(fname,x);
 g     = gradiente(fname,x);
 ng    = norm(g);
-p = 1; s = -1;
-% parte iterativa
+p = 1;
+% Parte iterativa
 while ( ng > tol && iter < maxiter &&  norm(p)> 1.e-04)
-     %p = -g;             % máximo descenso
+     %p = -g;             % Máximo descenso
      H = hessiana(fname,x);
-     p = -H\g;            % dirección de Newton
+     p = -H\g;            % Dirección de Newton
      
     %-----------------------------------------
     % Búsqueda de línea
-    alfa  = 1;              % paso completo
-    xt = x + alfa*p;        % primer punto de prueba
-    f  = feval(fname,x);  % valor de la función
-    f1 = feval(fname, xt);% valor de la función en el punto de prueba
-    s  = p'*g;            % derivada direccional
-    jter = 0;             % iteraciones internas
+    alfa  = 1;              % Paso completo
+    alfa2  = 1;              % Paso completo
+    xt = x + alfa*p;        % Primer punto de prueba
+    f  = feval(fname,x);  % Valor de la función
+    f1 = feval(fname, xt);% Valor de la función en el punto de prueba
+    f2 = f1;              % Valor de la función en el punto de prueba
+    s  = p'*g;            % Derivada direccional
+    jter = 0;             % Iteraciones internas
     
     if ( abs(s) < 1.e-06)
         disp('No existe suficiente descenso  ')
         disp(sprintf('%2.0f  %2.10f',iter, s   ))
         iter = maxiter; 
     end
-    while( (f1>f+alfa*c1*s) && jter < maxjter)  % búsqueda de línea
+    % Búsqueda de línea
+    while(f1>f+alfa*c1*s && f2>f+alfa2*c1*s && jter < maxjter)
+       %Backtracking
        alfa = alfa/2;
+       
+       %Interpolación cuadrática
+       d2 = f2 - f - s;
+       alfa2 = -( s)/(2*d2);
+       
+       %Selección de alfa
+       if (f1<=f+alfa*c1*s)
+           if(f2<=f+alfa2*c1*s)
+               %Backtracking e interpolación cuadrática cumplen Wolfe 1
+               alfa = max(alfa,alfa2);
+           end
+       else
+       %Sólo interpolación cuadrática cumple Wolfe1
+           if(f2<=f+alfa2*c1*s)
+               alfa = alfa2;
+           end
+       end
+       
+       %Actualización
        xt = x + alfa*p;
-       f1 = feval(fname, xt);% valor de la función en el punto de prueba
+       f1 = feval(fname, xt);
+       xt = x + alfa2*p;
+       f2 = feval(fname, xt);
        jter = jter +1;
-    end   
+       
+    end
+    
+   if (jter >= 20 || norm(alfa*p) < 1e-3)
+    alfa = 1e-2;
+   end
+   
     % Fin de búsqueda de línea
     %----------------------------------------------------------------------
     
@@ -81,8 +112,7 @@ while ( ng > tol && iter < maxiter &&  norm(p)> 1.e-04)
      fx = feval(fname,x); 
      g = gradiente(fname,x);
      iter = iter + 1;
-end       % Fin del while principal
-
+end
 
 end
 
